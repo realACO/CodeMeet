fb8f7adc17b21a14f2076b3004b62d6cb63d08d6
realACO <ashishoomman@gmail.com>
2026-03-05 03:39:18 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "success api is running" });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is running on port ", ENV.PORT);
+});
