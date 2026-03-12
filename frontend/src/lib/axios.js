f34401f9c47244befa31791deb19543ee485351e
realACO <ashishoomman@gmail.com>
2026-03-12 15:41:47 +0530

    minor errors

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { VITE_API_URL } from "../.env";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, //browser wills end the cookies to server auto matically with every single request
